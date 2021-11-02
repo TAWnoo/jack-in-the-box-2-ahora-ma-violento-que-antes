8822c46eafda716e96d005a438ae06f90e5bb028
zarbo-ian <58311312+zarbo-ian@users.noreply.github.com>
2021-11-02 01:23:06 -0300

    gfnfc

--- Assets/Scripts/Admin2.cs
+++ Assets/Scripts/Admin2.cs
@@ -18,32 +18,29 @@ public class Admin2 : MonoBehaviour
     }
 
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        cambioescena4();
+    }
+
     public void cambioescena1()
     {
-
         SceneManager.LoadScene("MainMenu");
-
     }
 
     public void cambioescena2()
     {
-
         SceneManager.LoadScene("Nivel_1.1");
-
     }
 
     public void cambioescena3()
     {
-
         SceneManager.LoadScene("fin");
-
     }
 
     public void cambioescena4()
     {
-
         SceneManager.LoadScene("Nivel_2.1");
-
     }
 }
 
